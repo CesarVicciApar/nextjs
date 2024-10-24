6a929f3b6cb972764149bf02f082f773ee1c20d0
CesarVicciApar <cesar.vicci@maihuechile.cl>
2024-10-23 22:31:16 -0300

    customers section added

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,11 +1,33 @@
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
 
-export default function Page() {
-    return <p>Customers</p>;
-  }
-  
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+
+  const fetchFilterCustomers = await fetchFilteredCustomers(query);
+
+  return (
+    <>
+      <Suspense
+        //si el query o el currentPage cambian, React "olvidará" el estado anterior de ese componente y lo volverá a montar como si fuera uno nuevo
+        key={query} 
+        fallback={<InvoicesTableSkeleton />}
+      >
+        <CustomersTable customers={fetchFilterCustomers} />
+      </Suspense>
+    </>
+  );
+}
